faae116db8993b0bf6200eb8d73760bd3dbfcb26
bertrand <bertrand.maubert.info@gmail.com>
2022-04-24 12:11:59 +0200

    Add animation feature
    I Introduced a Nonevaluated class in order to write non python-evaluated code in openSCAD. I introduced a prologue argument in write _BaseObject().write(). Gave an example of animation in animation repertory.

--- animation/animation_example.py
+++ animation/animation_example.py
@@ -1,15 +1,15 @@
 # Example : Create an OpenSCAD animation directly from Python
 
-# The animation functionality is based simply on aa OpenSCAD variable $t
+# The animation functionality is based simply on an OpenSCAD variable $t
 # that is changed automatically by OpenSCAD while repeatedly showing the model.
 # To activate animation, select "View->Animate" from the OpenSCAD  menu;
-# this will cause three fields to appear
-# underneath the Preview console: Time, FPS & Steps.
+# this will cause three fields to appear underneath the Preview console: Time, FPS & Steps.
+
 # To commence animation, enter values into the FPS and Steps input
 # fields (e.g. 50 FPS and 100 Steps for this animation).
-# This is not intended to directly produce real-time animations
-# but the image sequence can be exported to generate videos of
-# the animation.
+
+# Although this is not intended to directly produce real-time animations
+# but the image sequence can be exported to generate videos of the animation.
 
 from openpyscad import *
 
@@ -36,10 +36,7 @@ function position(time) = time < 0.5
 """
 
 
-
-
 U = Cube([15, 10, 50])
 U += Cylinder(60, r=30).translate([Nonevaluated("position($t)"), -25, -30])
 
 U.write("animated_cylinder.scad", prologue=scad_str)
-
